c19caa1182ba2c8729ce51a949a25b6321a1fd31
ohidurgclan <ohidur.ru.2019@gmail.com>
2021-12-30 21:09:16 +0600

    login,ragister

--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,46 +1,39 @@
-import { Alert, Button, Container, Typography } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
+import { Button, Container, FormControl, TextField, Typography } from '@mui/material';
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { NavLink } from 'react-router-dom';
-import { useLocation, useNavigate } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
 import Header from '../../Shared/Header/Header';
+import Footer from '../../Shared/Footer/Footer';
 
 const Login = () => {
-   const { user, loginUser, loading, signInWithGoogle, authError} = useAuth();
-    const history = useNavigate();
-    const location = useLocation();
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        loginUser(data?.email, data?.password, location, history);
-  };
-    const handleGoogleLogin = (data) => {
-        signInWithGoogle(location, history)
-    };  
   return (
-        <>
-            <Header />
-                <Container>
-                    <Typography sx={{ fontFamily: 'ubuntu', fontWeight: '500', color: '#0a2c3d', mt: 5, mb: 5 }} variant="h4">*Please Login</Typography>
-                    {user?.email && <Alert style={{ width: '37.8%' }} severity="success">Log in Successfully</Alert>}
-                    {authError && <Alert style={{ width: '37.8%' }} severity="error">{authError}</Alert>}
-                    {!loading && <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                        <Typography sx={{ fontFamily: 'ubuntu', fontWeight: '500', color: '#0a2c3d' }} variant="h5">Your Email</Typography>
-                        <input type="email" placeholder="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
-                        <Typography sx={{ fontFamily: 'ubuntu', fontWeight: '500', color: '#0a2c3d' }} variant="h5">Your Password</Typography>
-                        <input type="password" placeholder="Password" {...register("password", {required: true})} />
-
-                        <Button style={{ width: '12%', height: '2.5rem', backgroundColor: '#ff5a3c', color: '#fff', marginTop: '4rem',}} type="submit">Login</Button>
-                    </form>}
-                        {loading && <CircularProgress style={{color: '#ff5a3c'}} />}
-                    <NavLink style={{ textDecoration: 'none' }} to="/signup">
-                        <Button style={{ color: '#0a2c3d', fontSize: '1.1rem', marginBottom: '2rem'}} variant="text" type="submit">New User? Please Sign Up</Button>
-                    </NavLink>
-                    <Typography sx={{ fontFamily: 'ubuntu', fontWeight: '500', color: '#0a2c3d' }} variant="h5">- - - - - - - - - - - - - - - - - OR - - - - - - - - - - - - - - - - -</Typography>
-                    <Button onClick={handleGoogleLogin} style={{ width: '20%', height: '2.5rem', backgroundColor: '#ff5a3c', color: '#fff', marginTop: '3rem', marginBottom: '2rem' }} type="submit">Login With Google</Button>
-                </Container>
-        </>
+    <>
+    <Header />
+        <Container>
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 5, mb:3 }} variant="h5">Please Login</Typography>      
+        <FormControl sx={{ width:'40%' }}>
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 3, mb:3 }} variant="h6">Your Email</Typography> 
+        <TextField
+            required
+            fullWidth
+            margin="normal"          
+            type="email"          
+            id="outlined-required"
+        />          
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 3 , mb:3 }} variant="h6">Your Password</Typography> 
+        <TextField
+            required
+            fullWidth
+            margin="normal"          
+            type="password"        
+            id="outlined-required"
+        />
+        <Button sx={{ backgroundColor: 'var(--bg-black-100)', color: '#202020', textDecoration: 'none', width: '30%', mt:5 }} variant="contained">Login</Button>      
+        </FormControl>
+                <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 5 , mb:5 }} variant="h5">- - - - - - - - - - - - - - - - - OR - - - - - - - - - - - - - - - - -</Typography>       
+        <Button sx={{ backgroundColor: 'var(--bg-black-100)', color: '#202020', textDecoration: 'none' }} variant="contained">Login With Google</Button>      
+    </Container>
+    <Footer />
+    </>
     );
 };
 
--- src/Pages/Auth/Register/Register.js
+++ src/Pages/Auth/Register/Register.js
@@ -1,12 +1,39 @@
+import { Button, Container, FormControl, TextField, Typography } from '@mui/material';
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 import Header from '../../Shared/Header/Header';
+import Footer from '../../Shared/Footer/Footer';
 
 const Register = () => {
-    return (
-        <>
-          <Header />
-          <h2>i am register</h2>  
-        </>
+return (
+    <>
+    <Header />
+        <Container>
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 5, mb:3 }} variant="h5">Please Register</Typography>      
+        <FormControl sx={{ width:'40%' }}>
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 3, mb:3 }} variant="h6">Your Email</Typography> 
+        <TextField
+            required
+            fullWidth
+            margin="normal"          
+            type="email"          
+            id="outlined-required"
+        />          
+        <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 3 , mb:3 }} variant="h6">Your Password</Typography> 
+        <TextField
+            required
+            fullWidth
+            margin="normal"          
+            type="password"        
+            id="outlined-required"
+        />
+        <Button sx={{ backgroundColor: 'var(--bg-black-100)', color: '#202020', textDecoration: 'none', width: '30%', mt:5 }} variant="contained">Login</Button>      
+        </FormControl>
+                <Typography sx={{ fontWeight: '600', color: '#0a2c3d', mt: 5 , mb:5 }} variant="h5">- - - - - - - - - - - - - - - - - OR - - - - - - - - - - - - - - - - -</Typography>       
+        <Button sx={{ backgroundColor: 'var(--bg-black-100)', color: '#202020', textDecoration: 'none' }} variant="contained">Login With Google</Button>      
+    </Container>
+    <Footer />
+    </>
     );
 };
 
